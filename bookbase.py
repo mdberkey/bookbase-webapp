96a9123559fd69f40263f24b1849744edf26e50f
mdberkey <mberkey@wisc.edu>
2021-07-24 16:35:46 -0500

    Complete basic bookbase webapp

--- bookbase.py
+++ bookbase.py
@@ -4,28 +4,58 @@ from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 db_dir = os.path.join(project_dir, 'bookdatabase.db')
-database_file = f'sqlite://{db_dir}'
+database_file = f'sqlite:///{db_dir}'
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Book(db.Model):
     title = db.Column(db.String(80), unique=True, nullable=False,
-                      primary_keyA=True)
+                      primary_key=True)
 
     def __repr__(self):
         return f'<Title: {self.title}>'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=["GET", "POST"])
 def home():
+    books = None
     if request.form:
-        book = Book(title=request.form.get('title'))
-        db.session.add(book)
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add book")
+            print(e)
+    books = Book.query.all()
+    return render_template("home.html", books=books)
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        book = Book.query.filter_by(title=oldtitle).first()
+        book.title = newtitle
         db.session.commit()
-    return render_template('home.html')
+    except Exception as e:
+        print("Couldn't update book title")
+        print(e)
+    return redirect("/")
+
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
 
 
 if __name__ == '__main__':
